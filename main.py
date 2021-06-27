f591addc9ad351d68b5bc6a514c3cba48d228289
Allen Xu <allenyxu@users.noreply.github.com>
2021-06-27 13:23:02 -0700

    snake page added, embed once finished

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import random, requests
 app = Flask(__name__)
 
 backgrounds = ["https://cdn.discordapp.com/attachments/593331093130838016/858477082550730762/x0t55697o4z61_1.jpeg", "https://cdn.discordapp.com/attachments/593331093130838016/858477132388237329/x0t55697o4z61.jpeg"]
-
+monkeybackgrounds = ["https://i.pinimg.com/originals/1b/46/1a/1b461ad3fe0034b06138b88299d842dd.jpg", "https://thumbs.dreamstime.com/b/cute-cartoon-baby-monkey-banana-hugging-kawaii-character-drawing-isolated-vector-clip-art-illustration-142243904.jpg"]
 @app.route('/')
 def index():
-    response = requests.get('https://nekos.life/api/v2/img/wallpaper')
-    background = response.json()['url']
-    #background = random.choice(backgrounds)
+    #response = requests.get('https://nekos.life/api/v2/img/wallpaper')
+    #background = response.json()['url']
+    background = random.choice(monkeybackgrounds)
     return render_template("home.html", background=background)
 
 @app.route('/dino')
